66342bca4ecffdfed1293dd5b08a6fa4178e45d4
CyzmiX x <itzmedigamingx@gmail.com>
2023-11-07 11:20:50 +0000

    readme

--- target/debug/deps/ez_rust-ead88f2ae160f846.d
+++
@@ -1,8 +0,0 @@
-/workspace/ez-rust/target/debug/deps/ez_rust-ead88f2ae160f846.rmeta: src/main.rs src/ez.rs src/ez/io.rs src/ez/fs.rs
-
-/workspace/ez-rust/target/debug/deps/ez_rust-ead88f2ae160f846.d: src/main.rs src/ez.rs src/ez/io.rs src/ez/fs.rs
-
-src/main.rs:
-src/ez.rs:
-src/ez/io.rs:
-src/ez/fs.rs:
