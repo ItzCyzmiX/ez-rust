ba58ec006b352db0bfbbcb5909d9f7b384845bbf
CyzmiX x <itzmedigamingx@gmail.com>
2023-11-07 13:26:02 +0000

    exclude target

--- src/fs.rs
+++ src/fs.rs
@@ -3,7 +3,7 @@ pub fn read_file(path: &str) -> String {
 
     let content = fs::read_to_string(path)
                                             .expect("Error while reading file!");
-
+                                        
     return content.to_string();
 }
 
